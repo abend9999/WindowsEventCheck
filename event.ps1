388df52817f22654d71fb0430570981721609709
abend9999 <git@abend.dip.jp>
2017-08-23 21:21:18 +0900

    コメント追加

--- event.ps1
+++ event.ps1
@@ -1,13 +1,21 @@
 ﻿$now = [system.datetime]
 $beforetime = [system.datetime]
 
+#監視対象のアカウント名
 $Moniter_Users = @("Administrator","administrator")
+
+#監視対象外のIPアドレス
 $Exculde_Host = @("","-","127.0.0.1","192.168.111.10")
+
+#通知するSlack WebHook
 $Slack_Url = "https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXXXXX"
 
 $now = Get-Date
+
+#5分間分のログをチェックする。
 $beforetime = $now.addseconds(-300)
 
+#Slackへの通知
 Function sendSlack($mes){
     $enc = [System.Text.Encoding]::GetEncoding('ISO-8859-1')
     $utf8Bytes = [System.Text.Encoding]::UTF8.GetBytes($mes)
@@ -20,6 +28,7 @@ Function sendSlack($mes){
     Invoke-RestMethod -Uri $Slack_Url -Method Post -Body (ConvertTo-Json $notificationPayload)
 }
 
+#セキュリティ　イベントログからイベントID：4624を取得
 $eventlist = Get-Eventlog security -after $beforetime | Where-Object {$_.eventid -eq 4624}
 
 foreach($event in $eventlist){
@@ -32,6 +41,7 @@ foreach($event in $eventlist){
     $suspectuser = ""
 
     foreach($line in $detail){
+        #セキュリティIDからアカウント名を取得
         if($line -match "Security ID:"){
             $tmp = $line.split(":")
             $tmp_secid = $tmp[1].trim()
@@ -50,6 +60,7 @@ foreach($event in $eventlist){
             }
         }
 
+        #イベントログにおけるアカウント名
         if($line -match "Account Name:"){
             $tmp = $line.split(":")
             $tmp_account = $tmp[1].trim()
@@ -59,6 +70,7 @@ foreach($event in $eventlist){
             }
         }
 
+        #ログオンID
         if($line -match "Logon ID:"){
             $tmp = $line.split(":")
             $tmp_logonid = $tmp[1].trim()
@@ -68,6 +80,7 @@ foreach($event in $eventlist){
             }
         }
 
+		#アクセス元IPアドレス
         if($line -match "Source Network Address:"){
             $tmp = $line.split(":")
             $src = $tmp[1].trim()
@@ -81,12 +94,15 @@ foreach($event in $eventlist){
             }
 
             if($flg){
-		$send_mes = ""
-		
+                $send_mes = ""
+
                 if(!$principalname.contains($suspectuser)){
                     $send_mes = "SurityID and Username are different.  Suspicious Access!!!!`n`n`n"
                 }
+                
+                #通知内容
                 $send_mes = $send_mes + "<<<<<<<  Login Info  >>>>>>>`n`nTime:   " + $event.TimeGenerated + "`nLogonID:    " + $logonid + "`nFrom:   " + $src + "`nAccount:   " + $suspectuser + "`nSecurityID:    " + $principalname +"`n"
+                #Slackへの通知
                 sendSlack($send_mes)
             }
 
